b8efbcf5d60a891b1d311a5988f5cab5c80d9608
hrsnr <159692674+hrsnr@users.noreply.github.com>
2026-09-13 12:39:52 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'spiele-dashboard-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './manifest.json',
+  './spiele/wizard.html',
+  './spiele/flip7.html',
+  './spiele/moelkky.html',
+  './spiele/minigolf.html',
+  './spiele/flappy_henri.html'
+];
+
+// Installation & Caching aller Dateien
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Anfragen im Offline-Fall direkt aus dem Cache bedienen
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
